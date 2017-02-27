81f1e2fed2953a9544c044d30ae6075d2596fe22
mervesimsek <mervsimsekk@yandex.com>
2017-02-27 18:05:36 +0000

    addcarscreen

--- app/src/main/java/com/example/mervesimsek/vehicleapp/MainActivity.java
+++ app/src/main/java/com/example/mervesimsek/vehicleapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mervesimsek.vehicleapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,11 +81,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_car) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_add) {
 
-        } else if (id == R.id.nav_slideshow) {
+            Intent intentadd = new Intent(MainActivity.this,AddCarActivity.class);
+            startActivity(intentadd);
+
+        } else if (id == R.id.nav_aboutappsims) {
 
         } else if (id == R.id.nav_share) {
 
