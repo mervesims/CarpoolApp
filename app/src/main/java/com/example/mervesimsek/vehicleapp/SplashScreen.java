96d4d0b5db01f9757bf6071432520834cc7d1b59
mervesimsek <mervsimsekk@yandex.com>
2017-02-27 16:15:18 +0000

    splashscreen

---
+++ app/src/main/java/com/example/mervesimsek/vehicleapp/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.mervesimsek.vehicleapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mnmlondon2 on 27/02/2017.
+ */
+
+public class SplashScreen extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splashscreen);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                    startActivity(intent);
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+
+        };
+        thread.start();
+    }
+}
