d4a71464e1c4b5268f8cee0824a3ccf5797406e6
mervesimsek <mervsimsekk@yandex.com>
2017-04-04 10:51:26 +0100

    Main activity code review

--- app/src/main/java/com/example/mervesimsek/vehicleapp/MainActivity.java
+++ app/src/main/java/com/example/mervesimsek/vehicleapp/MainActivity.java
@@ -24,17 +24,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-
+/**
+ * Kullanıcının splash screendn sonra karşısına çıkacak ilk ekrandır.
+ * Navigation Drawer işlemleri bu ekrandadır.
+ */
 
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener
+{
     final Context context = this;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -42,123 +48,116 @@ public class MainActivity extends AppCompatActivity
         Typeface font = Typeface.createFromAsset(context.getAssets(), "fonts/gist.ttf");
         logo.setTypeface(font);
 
-        //TODO : Sirali floating action button yaratma.
+        //TODO :Sirali floating action button yaratma.
         ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
         drawable.getPaint().setColor(getResources().getColor(R.color.buttonpressed2));
+            final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
+            actionA.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intcars = new Intent(MainActivity.this, ItemListActivity.class);
+                    startActivity(intcars);
+                }
+            });
+            final FloatingActionButton actionB = (FloatingActionButton) findViewById(R.id.action_b);
+            actionB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intgo = new Intent(MainActivity.this, AddCarActivity.class);
+                    startActivity(intgo);
+                }
+            });
 
-
-
-        final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
-
-        actionA.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intcars = new Intent(MainActivity.this, ItemListActivity.class);
-                startActivity(intcars);
-            }
-        });
-
-        final FloatingActionButton actionB = (FloatingActionButton) findViewById(R.id.action_b);
-        actionB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intgo = new Intent(MainActivity.this, AddCarActivity.class);
-                startActivity(intgo);
-            }
-
-        });
-
-
+        //TODO : Ekranın herhangi bir yerine dokunulduğunda drawer kapatma
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
+    //TODO : Navigation Drawer Kapatma
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+        {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
+        }
+        else
+        {
             super.onBackPressed();
         }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         return true;
     }
 
+    //TODO : Setting menüsü yaratma
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
+    //TODO: Navigation menüye tıklandığında yapılacak işlemler ve içerik
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
         int id = item.getItemId();
 
-        if(id == R.id.nav_main) {
-
+        if(id == R.id.nav_main)
+        {
             Intent intentmain = new Intent(MainActivity.this, MainActivity.class);
             startActivity(intentmain);
-
-        } else if (id == R.id.nav_car) {
-
+        }
+        else if (id == R.id.nav_car)
+        {
             Intent intentitem = new Intent(MainActivity.this, ItemListActivity.class);
             startActivity(intentitem);
-
-        } else if (id == R.id.nav_add) {
-
+        }
+        else if (id == R.id.nav_add)
+        {
             Intent intentadd = new Intent(MainActivity.this,AddCarActivity.class);
             startActivity(intentadd);
-
-        } else if (id == R.id.nav_aboutappsims) {
+        }
+        else if (id == R.id.nav_aboutappsims)
+        {
             Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.appsims.com"));
             startActivity(browser);
-
-        } else if (id == R.id.nav_share) {
-            String message = " Carpool App ";
-            Intent share = new Intent(Intent.ACTION_SEND);
-            share.setType("text/plain");
-            share.putExtra(Intent.EXTRA_TEXT, message);
-
-            startActivity(Intent.createChooser(share, "Carpool"));
-
-        } else if (id == R.id.nav_send) {
-
-            String message = " Carpool App ";
-            Intent share = new Intent(Intent.ACTION_SEND);
-            share.setType("text/plain");
-            share.putExtra(Intent.EXTRA_TEXT, message);
-
-            startActivity(Intent.createChooser(share, "Carpool"));
-
         }
-
+        else if (id == R.id.nav_share)
+        {
+            String message = " Carpool App ";
+            Intent share = new Intent(Intent.ACTION_SEND);
+            share.setType("text/plain");
+            share.putExtra(Intent.EXTRA_TEXT, message);
+            startActivity(Intent.createChooser(share, "Carpool"));
+        }
+        else if (id == R.id.nav_send)
+        {
+            String message = " Carpool App ";
+            Intent share = new Intent(Intent.ACTION_SEND);
+            share.setType("text/plain");
+            share.putExtra(Intent.EXTRA_TEXT, message);
+            startActivity(Intent.createChooser(share, "Carpool"));
+        }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-
-
 }
